8dce87d425f0fc2c34f0bd804a3b17b6101e5ee2
AlexandreRCS <ale.maresias@gmail.com>
2024-03-11 22:42:37 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.json())
+
 const PORT = 3333
 
 app.listen(PORT, ( ) => console.log(`Server is running on PORT ${PORT}`))
