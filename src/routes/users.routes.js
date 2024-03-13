fae6fa9436733680e5a6db1f1eb2a5fca9b8f5d3
AlexandreRCS <ale.maresias@gmail.com>
2024-03-12 21:31:01 -0300

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express")
 
+const UsersController = require("../controllers/UsersController")
+
 const userRouters = Router()
+const usersController = new UsersController()
 
 
-userRouters.post("/", (request, response) => {
-
-})
+userRouters.post("/", usersController)
 
 
 module.exports = userRouters
