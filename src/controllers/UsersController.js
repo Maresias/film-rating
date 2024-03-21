5c64f85930d9e4b5c50d8b92b84eef8eaa23a068
AlexandreRCS <ale.maresias@gmail.com>
2024-03-20 22:46:40 -0300

    updating user data

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -24,7 +24,36 @@ class UsersController {
         return response.status(201).json()
     }
 
+    async update(request, response){
+        const {name, email, password } = request.body
+        const {id} = request.params
 
+        const database = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+        if(!user){
+            throw new AppError("Usuário não encontrado")
+        }
+
+        const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if(userWithUpdateEmail && userWithUpdateEmail.id !== id){
+            throw new AppError("Este e-mail já está em uso. ")
+        }
+
+        user.name = name
+        user.email = email
+        user.password = await hash(password, 8)
+
+        await database.run(
+            `UPDATE users SET
+            name = ?,
+            email = ?,
+            password = ?,
+            updated_at = ?`,
+            [user.name, user.email, user.password, new Date(), id]
+        )
+
+    }
 }
 
 
