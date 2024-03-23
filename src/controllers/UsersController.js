34308f59c0085d77a721f7dc94df2eeafe73a2fe
AlexandreRCS <ale.maresias@gmail.com>
2024-03-22 21:56:30 -0300

    validating email

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -42,8 +42,8 @@ class UsersController {
             throw new AppError("Este e-mail já está em uso. ")
         }
 
-        user.name = name
-        user.email = email
+        user.name = name ?? user.name
+        user.email = email ?? user.email
 
         if (password && !old_password){
             throw new AppError("Você precisa informar a senha antiga para definir a nova senha. ")
