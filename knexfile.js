5546c3ce51dc25d1b40b473a94252718dc4ca951
AlexandreRCS <ale.maresias@gmail.com>
2024-03-23 21:51:36 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,11 @@
+const path = require("path")
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
-    }
+    },
+    useNullAsDefault: true
   }
 };
