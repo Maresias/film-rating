e99ce5c02a5182695989e984de03f314a969f556
AlexandreRCS <ale.maresias@gmail.com>
2024-03-25 20:46:20 -0300

    Update 20240325233619_createMovie.js

--- src/database/knex/migrations/20240325233619_createMovie.js
+++ src/database/knex/migrations/20240325233619_createMovie.js
@@ -1,8 +1,11 @@
 
-exports.up = knex => knex.schema{
-  
-};
+exports.up = knex => knex.schema.createTable("movie", table =>{
+    table.increments("id")
+    table.text("title")
+    table.text("description")
 
-exports.down = function(knex) {
-  
-};
+    table.timestamp("created_at").default(knex.fn.now())
+    table.timestamp("updated_at").default(knex.fn.now())
+})
+
+exports.down = kenx => knex.schema.dropTable("movie")
