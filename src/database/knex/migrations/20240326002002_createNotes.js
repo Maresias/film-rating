f3ad8198491045f68694b0dcccdccb77029c1068
AlexandreRCS <ale.maresias@gmail.com>
2024-03-25 21:50:27 -0300

    createnotes

--- src/database/knex/migrations/20240326002002_createNotes.js
+++ src/database/knex/migrations/20240326002002_createNotes.js
@@ -1,8 +1,12 @@
 
-exports.up = function(knex) {
-  
-};
+exports.up = knex => knex.schema.createTable("notes", table => {
+    table.increments("id")
+    table.integer("rating").notNullable()
+    table.integer("user_id").references("id").inTable("users")
+    table.integer("movie_id").references("id").inTable("movie")
 
-exports.down = function(knex) {
-  
-};
+    table.timestamp("created_at").default(knex.fn.now())
+    table.timestamp("updated_").default(knex.fn.now())
+})
+
+exports.down = knex => knex.schema.dropTable("notes")
