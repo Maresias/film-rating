6d6f3b009719009c139756ac2af887ebf07aa6ac
AlexandreRCS <ale.maresias@gmail.com>
2024-03-25 23:44:44 -0300

    update

--- src/database/knex/migrations/20240325233619_createMovie.js
+++ src/database/knex/migrations/20240325233619_createMovie.js
@@ -5,7 +5,6 @@ exports.up = knex => knex.schema.createTable("movie", table =>{
     table.text("description")
 
     table.timestamp("created_at").default(knex.fn.now())
-    table.timestamp("updated_at").default(knex.fn.now())
 })
 
 exports.down = kenx => knex.schema.dropTable("movie")
