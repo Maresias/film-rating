8c79d9a9bea63b13dcbabb0a9f1068fd0343396c
AlexandreRCS <ale.maresias@gmail.com>
2024-03-25 22:21:26 -0300

    Update 20240326010900_createTags.js

--- src/database/knex/migrations/20240326010900_createTags.js
+++ src/database/knex/migrations/20240326010900_createTags.js
@@ -1,4 +1,11 @@
 
-exports.up = function(knex) 
+exports.up = knex => knex.schema.createTable("tags", table =>{
+    table.increments("id")
+    table.text("name").notNullable()
 
-exports.down = function(knex) 
+    table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE")
+    table.integer("user_id").references("id").inTable("users")
+    table.integer("movie_id").references("id").inTable("movie")
+})
+
+exports.down = knex => knex.schema.dropTable("tags")
