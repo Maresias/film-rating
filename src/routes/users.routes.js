7ad1cdb64252f9004dcc446c44ede7d23700d719
AlexandreRCS <ale.maresias@gmail.com>
2024-03-26 21:50:46 -0300

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,12 +5,13 @@ const MovieController = require("../controllers/MovieController")
 
 const userRouters = Router()
 const usersController = new UsersController()
+const movieController = new MovieController()
 
 
 userRouters.post("/", usersController.create)
 userRouters.put("/:id", usersController.update)
 
-
+userRouters.post("/",movieController.create)
 
 
 module.exports = userRouters
