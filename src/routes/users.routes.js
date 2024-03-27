6027bcbc68d48c34d19db1ceac104ce29b06a5e5
AlexandreRCS <ale.maresias@gmail.com>
2024-03-26 22:05:04 -0300

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,17 +1,17 @@
 const { Router } = require("express")
 
 const UsersController = require("../controllers/UsersController")
-const MovieController = require("../controllers/MovieController")
+
 
 const userRouters = Router()
 const usersController = new UsersController()
-const movieController = new MovieController()
+
 
 
 userRouters.post("/", usersController.create)
 userRouters.put("/:id", usersController.update)
 
-userRouters.post("/",movieController.create)
+
 
 
 module.exports = userRouters
