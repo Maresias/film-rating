90f5cc6c9f15cf7591277bf094bd1ff3216bbfb7
AlexandreRCS <ale.maresias@gmail.com>
2024-03-26 21:20:12 -0300

    Update MovieController.js

--- src/controllers/MovieController.js
+++ src/controllers/MovieController.js
@@ -2,6 +2,7 @@ const sqliteConnection = require("../database/sqlite")
 
 class MovieController {
     async create(request, response){
+        const { title, description } = request.body
         
     }
 }
