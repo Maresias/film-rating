9a4aec890aea88bfda234ccc402d5bb286ef4075
AlexandreRCS <ale.maresias@gmail.com>
2024-03-27 22:54:51 -0300

    Update NotesController.js

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -3,5 +3,13 @@ const knex = require("../database/knex")
 class NotesController {
     async create(request, response){
         const { rating, tags } = request.body
+        const { user_id, movie_id } = request.params
+
+        const note_id = await knex("notes").insert({
+            rating,
+            user_id,
+            movie_id
+        })
+
     }
 }
