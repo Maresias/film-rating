ca35a821fa513a92b454cba623a36cdec4f223d8
AlexandreRCS <ale.maresias@gmail.com>
2024-03-30 17:12:15 -0300

    Update NotesController.js

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -7,6 +7,10 @@ class NotesController {
         const { user_id, movie_id } = request.params
 
         const filmWasRated = await knex("notes").where({user_id: user_id} && {movie_id: movie_id})
+        
+        if(filmWasRated.length !== 0){
+            throw new AppError("Filme já avaliado pelo úsuario, caso queira você pode atualizar sua nota")
+        }
 
         if(tags.length === 0 || rating.length === 0){
             throw new AppError("E Necessário 1 Tag e uma Nota")
@@ -19,9 +23,6 @@ class NotesController {
             throw new AppError("A nota no filme deve está entre 1 e 5")
         }
 
-        if(filmWasRated.length !== 0){
-            throw new AppError("Filme já avaliado pelo úsuario, caso queira você pode atualizar sua nota")
-        }
 
 
         const [note_id] = await knex("notes").insert({
