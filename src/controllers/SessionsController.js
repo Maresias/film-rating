0f7ebfc53660ea3ca28d7a2972d1ac88e36cbc53
AlexandreRCS <ale.maresias@gmail.com>
2024-07-13 23:18:53 -0300

    Updade
    Validação do email / senha

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,6 @@
 const knex = require("../database/knex");
 const AppError = require("../Utils/AppError")
-
+const { compare } = require("bcryptjs")
 class SessionsController {
     async create(request, response) {
         const { email, password } = request.body
@@ -9,6 +9,12 @@ class SessionsController {
 
         if (!user) {
             throw new AppError("E-mail e/ou senha incorrenta",401)
+        }
+
+        const passwordMatched = await compare(password, user.password)
+
+        if (!passwordMatched) {
+            throw new AppError("E-mail e/ou senha incorreta", 401)
         }
 
         return response.json(user)
