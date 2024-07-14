7555bd1fe8eee279702a797f2315adf6e28540ca
AlexandreRCS <ale.maresias@gmail.com>
2024-07-13 22:40:47 -0300

    Update SessionsController.js

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,7 @@
 class SessionsController {
-    async create() {
-
+    async create(request, response) {
+        const { email, password } = request.body
+        return response.json({email, password})
     }
 }
 
