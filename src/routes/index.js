492fb3ca2c6b52733f26a4e5a71e608474c3bd21
AlexandreRCS <ale.maresias@gmail.com>
2024-07-13 22:31:20 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ const { Router } = require("express")
 const userRouter = require("./users.routes")
 const movieRouter = require("./movie.routes")
 const notesRouter = require("./notes.routes")
+const sessionsRoutes = require("./sessions.routes")
 
 const routes = Router()
 
 routes.use("/users", userRouter)
 routes.use("/movie", movieRouter)
 routes.use("/notes", notesRouter)
+routes.use("/sessions", sessionsRoutes)
 
 
 module.exports = routes
