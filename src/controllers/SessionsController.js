11fbfaa438ef247fc4933015a0554b49414daa79
AlexandreRCS <ale.maresias@gmail.com>
2024-07-13 23:09:43 -0300

    Update SessionsController.js

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,11 +1,15 @@
-const knex = require("../database/knex")
-const AppError = require("../utils/AppError")
+const knex = require("../database/knex");
+const AppError = require("../Utils/AppError")
 
 class SessionsController {
     async create(request, response) {
         const { email, password } = request.body
 
         const user = await knex("users").where({email}).first()
+
+        if (!user) {
+            throw new AppError("E-mail e/ou senha incorrenta",401)
+        }
 
         return response.json(user)
     }
