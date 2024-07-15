ba1e370c92290461f69dca7ac01313d95045d856
AlexandreRCS <ale.maresias@gmail.com>
2024-07-14 22:05:46 -0300

    Update SessionsController.js

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -25,8 +25,12 @@ class SessionsController {
         }
 
         const {secret, expiresIn} = authConfig.jwt
+        const token = sign({}, secret,{
+            subject: String(user.id),
+            expiresIn
+        })
 
-        return response.json(user)  
+        return response.json(user, token)  
     }
 }
 
