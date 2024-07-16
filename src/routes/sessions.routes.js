370d8cd48da49d3b9a8f8cd3e233ed35d85affc0
AlexandreRCS <ale.maresias@gmail.com>
2024-07-15 23:27:24 -0300

    Update

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -1,11 +1,10 @@
 const { Router } = require("express")
-const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const SessionsController = require("../controllers/SessionsController")
 const sessionsController = new SessionsController()
 
 const sessionsRoutes = Router()
 
-sessionsRoutes.post("/", ensureAuthenticated, sessionsController.create)
+sessionsRoutes.post("/", sessionsController.create)
 
 module.exports = sessionsRoutes
