1ad1ea199c4ecaca24620c5bf60c52b54b4a3806
AlexandreRCS <ale.maresias@gmail.com>
2024-07-15 22:55:02 -0300

    Update ensureAuthenticated.js

--- src/middlewares/ensureAuthenticated.js
+++ src/middlewares/ensureAuthenticated.js
@@ -1,3 +1,7 @@
 const { verify } = require("jsonwebtoken")
 const AppError = require("../Utils/AppError")
 const authConfig = require("../configs/auth")
+
+function ensureAuthenticated(request, response, next){
+    const authHeader = request.headers.authorization
+}
