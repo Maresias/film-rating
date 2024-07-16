8f0348cba32cdb48998b97ea8d349f3f457bb83a
AlexandreRCS <ale.maresias@gmail.com>
2024-07-15 23:29:46 -0300

    Update

--- src/middlewares/ensureAuthenticated.js
+++ src/middlewares/ensureAuthenticated.js
@@ -16,7 +16,7 @@ function ensureAuthenticated(request, response, next){
        request.user = {
         id: Number(user_id)
        }
-
+       
        return next()
     } catch{
         throw new AppError("JWT token inválido", 401)
