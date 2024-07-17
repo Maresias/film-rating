159ecea4aa45c27ab4f0b50d185a575209a2333b
AlexandreRCS <ale.maresias@gmail.com>
2024-07-16 22:48:42 -0300

    Update DiskStorage.js

--- src/providers/DiskStorage.js
+++ src/providers/DiskStorage.js
@@ -1,3 +1,16 @@
 const fs = require("fs")
 const path = require("path")
 const uploadsConfig = require("../configs/upload")
+
+class DiskStorage {
+    async saveFile(file) {
+        await fs.promises.rename(
+            path.resolve(uploadsConfig.TMP_FOLDER, file),
+            path.resolve(uploadsConfig.UPLOADS_FOLDER, file)
+        )
+
+        return file
+    }
+
+    
+}
