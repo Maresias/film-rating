decac0559fe41ace24e21f6ac95484584f0c2a24
AlexandreRCS <ale.maresias@gmail.com>
2024-07-16 22:28:50 -0300

    Update upload.js

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -1,4 +1,6 @@
 const path = require("path")
+const multer = require("multer")
 
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp")
 const UPLOADS_FOLDER = path.resolve(__dirname, "uploads")
+
