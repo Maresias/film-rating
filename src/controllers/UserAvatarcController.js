f78a9d547989f8a8b672ecb5f195e9438f80d2ed
AlexandreRCS <ale.maresias@gmail.com>
2024-07-17 22:22:54 -0300

    Update UserAvatarcController.js

--- src/controllers/UserAvatarcController.js
+++ src/controllers/UserAvatarcController.js
@@ -3,6 +3,18 @@ const AppError = require("../Utils/AppError")
 
 class UserAvatarController {
     async update(request, response){
+        const user_id = request.user.id 
+        const avatarFileName = request.file.filename 
+
+        const user = await knex("users")
+        .where({id: user_id}).first()
         
+        if (!user) {
+            throw new AppError("Somente usuários autenticados podem mudar a avatar")
+        }
+
+        if (user.avatar) {
+            
+        }
     }
 }
