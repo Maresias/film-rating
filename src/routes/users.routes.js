b0489f25804eab5772ca7c8732bdd990f08296db
AlexandreRCS <ale.maresias@gmail.com>
2024-07-17 21:50:49 -0300

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,16 +1,24 @@
 const { Router } = require("express")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
 
 const UsersController = require("../controllers/UsersController")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 
 const userRouters = Router()
+const upload = multer(uploadConfig.MULTER)
+
+
+
 const usersController = new UsersController()
-
-
 
 userRouters.post("/", usersController.create)
 userRouters.put("/", ensureAuthenticated, usersController.update)
+userRouters.patch("/avater", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+    console.log(request.file.filename)
+    response.json()
+})
 
 
 
