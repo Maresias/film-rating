f6a6e34e12e61c80d78b145a65c01a64da531022
AlexandreRCS <ale.maresias@gmail.com>
2024-07-17 22:27:26 -0300

    Update UserAvatarcController.js

--- src/controllers/UserAvatarcController.js
+++ src/controllers/UserAvatarcController.js
@@ -1,10 +1,13 @@
 const knex = require("../database/knex")
 const AppError = require("../Utils/AppError")
+const DiskStorage = require("../providers/DiskStorage")
 
 class UserAvatarController {
     async update(request, response){
         const user_id = request.user.id 
         const avatarFileName = request.file.filename 
+
+        const diskStorage = new DiskStorage()
 
         const user = await knex("users")
         .where({id: user_id}).first()
@@ -14,7 +17,11 @@ class UserAvatarController {
         }
 
         if (user.avatar) {
-            
+            await diskStorage.deleteFile(user.avatar)
         }
+
+        const filename = await diskStorage.saveFile(avatarFileName)
+
+        user.avatar = filename
     }
 }
