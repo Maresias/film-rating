9f32ad52272baccd0ac06b40ca815aea8d4e9bfb
AlexandreRCS <ale.maresias@gmail.com>
2024-08-21 22:15:51 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -37,5 +37,5 @@ app.use((error, request, response, next)=>{
     })
 })
 
-const PORT = process.env.SERVER_PORT
+const PORT = process.env.SERVER_PORT || 3333
 app.listen(PORT, ( ) => console.log(`Server is running on PORT ${PORT}`))
